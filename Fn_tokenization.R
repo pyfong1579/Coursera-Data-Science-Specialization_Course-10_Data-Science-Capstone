93db0783fb3c0a5baebbf09689a1229f02d0392d
P.Y.Fong <pyfong1@gmail.com>
2021-02-01 16:56:07 +0800

    Clean up of directory structures

--- Fn_tokenization.R
+++
@@ -1,9 +0,0 @@
-# tokenization function
-tokenize <- function(data, ngram, minfreq) {
-    token <- function(data) NGramTokenizer(data, Weka_control(min= ngram, max= ngram))
-    matrix <- TermDocumentMatrix(data, control= list(tokenize= token))
-    freq <- findFreqTerms(matrix, lowfreq= minfreq)
-    freqdata <- sort(rowSums(as.matrix(matrix[freq,])), decreasing= T)
-    freqdata <- data.frame(Phrase= names(freqdata), Frequency= freqdata, row.names = NULL)
-    return(freqdata)
-}
